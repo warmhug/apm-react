7296147838b0a41eacb07e5755c8fb31b3a7341e
warmhug <hualei5280@gmail.com>
2022-07-18 19:30:30 +0800

    feat: test sentry issue

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 const Home = props => {
-  
+  const methodDoesNotExist = () => {
+    throw new Error('自定义错误');
+  };
+
   return (
     <div>
       <h2>Home {console.log(props)}</h2>
       Initialized on 2022-05-27 <br/>
-      Use react@18.x and react-router@6.x <br/>
+      Use react@18.x and react-router@6.x 
+      <br/>
+      <br/>
 
-      <div>
-        <button onClick={methodDoesNotExist}>Break the world</button>
-      </div>
+      <button onClick={methodDoesNotExist}>Break the world</button>
     </div>
   );
 };
